f6f640334759dd7847d52d6eaf57254968b95b19
DineshEzhumalai <dineshkkrezhil12@gmail.com>
2023-03-31 17:40:11 +0530

    Initial Repository

--- src/main/java/com/example/netcreeper/HelloApplication.java
+++ src/main/java/com/example/netcreeper/HelloApplication.java
@@ -11,8 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Splashscreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("NetCreeper+");
         stage.setScene(scene);
         stage.initStyle(StageStyle.UNDECORATED);
